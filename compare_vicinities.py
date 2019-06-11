3879c3bf108251e1e76cfaed031ab2ba177abce1
ivysandberg <ins5kx@virginia.edu>
2019-06-11 14:24:30 -0400

    Define the pair score between minutiae

---
+++ compare_vicinities.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import itertools
+import math
+
+from yapf.yapflib.yapf_api import FormatFile
+FormatFile(
+    "/Users/ivysandberg/dmc/NXGBCC/dev/compare_vicinities.py", in_place=True)
+''' Compare Vicinities '''
+
+V1 = [(3, 2, 0.5), (-4, 1.2, 3.4), (5, -2, 4.4), (1, -1, 1), (3.6, 2, 0.5),
+      (1, 1, 1), (-2, -0.3, 0), (1, 2, -4)]
+
+V2 = [(2.2, 3.1, -1.2), (-2, 3, 1), (1, 1, 1), (1, -1, -1), (4, 0, 0)]
+
+#for i in list_of_minutiae:
+#print (i[2])
+
+for p, q in itertools.product(V1, V2):
+    print(p, q)
+
+    #for p, q in zip(V1, V2):   # doesn't match ALL possible combinations
+
+
+    def pairing_score(p, q):
+        #output = []
+        sd_x = 2.3
+        sd_y = 4.1
+        sd_theta = 1.2
+        score = math.exp((-((p[0] - q[0])**2) / sd_x)) * math.exp(
+            (-((p[1] - q[1])**2) / sd_y)) * math.exp(
+                (-((p[2] - q[2])**2) / sd_theta))
+        output = np.array(score)
+
+        #output.append(score)
+        return output
+
+    print(pairing_score(p, q))
+
+    # Create matrix P x Q of all the pairing scores
--- compress_vicinity_def.py
+++ compress_vicinity_def.py
@@ -3,50 +3,68 @@ import pandas as pd
 import math
 import itertools
 
+from yapf.yapflib.yapf_api import FormatFile
+FormatFile(
+    "/Users/ivysandberg/dmc/NXGBCC/dev/compress_vicinity_def.py",
+    in_place=True)
 
 # list of all the minutiae points
-list_of_minutiae = [(3,2,0.5), (-4,1.2,3.4), (5,-2,4.4), (1,-1,1), (3.6,2,0.5), (1,1,1), (-2,-0.3, 0), (1,2, -4)]
+list_of_minutiae = [(3, 2, 0.5), (-4, 1.2, 3.4), (5, -2, 4.4), (1, -1, 1),
+                    (3.6, 2, 0.5), (1, 1, 1), (-2, -0.3, 0), (1, 2, -4)]
 
 #print(itertools.combinations_with_replacement(list_of_minutiae, 2))
 
-for mi,mj in itertools.combinations_with_replacement(list_of_minutiae, 2):
-    def distance(mi,mj):
+for mi, mj in itertools.combinations_with_replacement(list_of_minutiae, 2):
+
+    def distance(mi, mj):
         dist = math.sqrt((mj[0] - mi[0])**2 + (mj[1] - mi[1])**2)
         return dist
+
     #print(mi, mj, "Distance :", dist)
     def unit_vector(list_of_minutiae):
-        return list_of_minutiae / np.linalg.norm(list_of_minutiae)  # this statement breaks when mj = (0,0,0)
+        return list_of_minutiae / np.linalg.norm(
+            list_of_minutiae)  # this statement breaks when mj = (0,0,0)
+
     #print(unit_vector(mi))j
     mi_u = unit_vector(mi)
     mj_u = unit_vector(mj)
 
-    def angle_between(mi,mj):
+    def angle_between(mi, mj):
         angle = np.arccos(np.clip(np.dot(mi_u, mj_u), -1.0, 1.0))
         return angle
+
     #print (angle)
 
-    print ("   ")
-    print (mi, mj)
-    print ("Distance: ", distance(mi,mj))
-    print ("Angle: ", angle_between(mi,mj))
+    # print ("   ")
+    # print (mi, mj)
+    # print ("Distance: ", distance(mi,mj))
+    # print ("Angle: ", angle_between(mi,mj))
 
     # define X coordinate of Vicinity 1 (corresponding to mi and mj)
-    X = distance(mi, mj) * (math.cos(angle_between(mi,mj) - mi[2]))
-    print("X: ", X)
+    X = distance(mi, mj) * (math.cos(angle_between(mi, mj) - mi[2]))
+    #print("X: ", X)
 
     # define Y coordinate
-    Y = distance(mi, mj) * (math.sin(angle_between(mi,mj)-mi[2]))
-    print("Y: ", Y)
+    Y = distance(mi, mj) * (math.sin(angle_between(mi, mj) - mi[2]))
+
+    #print("Y: ", Y)
 
     # define theta
     def theta(mi, mj):
         new_theta = mi[2] - mj[2]
         return new_theta
+
     theta = theta(mi, mj)
-    print("Theta: ", theta)
+
+    #print("Theta: ", theta)
+
 
     def vicinity(mi, mj):
         output = []
-        output.append([X, Y, theta])
+        output.append([X, Y, theta])  # creates the double brackets
+        #output = np.array([X, Y, theta])    # has no commas
         return output
-    print("Vicintiy: ", vicinity(mi, mj))
+
+    #print("Vicintiy: ", vicinity(mi, mj))
+
+    print(vicinity(mi, mj))
