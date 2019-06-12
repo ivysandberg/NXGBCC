636301eae9e78259bac0b2d671877dac98196145
ivysandberg <ins5kx@virginia.edu>
2019-06-12 17:25:19 -0400

    add Hungarian Algorithm

--- compare_vicinities.py
+++ compare_vicinities.py
@@ -2,35 +2,26 @@ import numpy as np
 import pandas as pd
 import itertools
 import math
+from munkres import Munkres, print_matrix
 
-# from yapf.yapflib.yapf_api import FormatFile
-# FormatFile(
-#     "/Users/ivysandberg/dmc/NXGBCC/dev/compare_vicinities.py", in_place=True)
-# ''' Compare Vicinities '''
+from yapf.yapflib.yapf_api import FormatFile
+FormatFile(
+    "/Users/ivysandberg/dmc/NXGBCC/dev/compare_vicinities.py", in_place=True)
+''' Compare Vicinities '''
 
 V1 = [(3, 2, 0.5), (-4, 1.2, 3.4), (5, -2, 4.4), (1, -1, 1), (3.6, 2, 0.5),
-      (1, 1, 1), (-2, -0.3, 0), (1, 2, -4)]
+      (1, 1, 1), (-2, -0.3, 0), (1, 2, -4), (0, 0, 0)]
 
-V2 = [(2.2, 3.1, -1.2), (-2, 3, 1), (1, 1, 1), (1, -1, -1), (4, 0, 0)]
+V2 = [(2.2, 3.1, -1.2), (-2, 3, 1), (1, 1, 1), (1, -1, -1), (4, 0, 0), (0, 0,
+                                                                        0)]
 
 #for i in list_of_minutiae:
 #print (i[2])
 
-# def compare_vicinities(Vi,Vj):
-#     output_matrix = np.zeros((len(Vi),len(Vj)))
-#     for i in range(len(Vi)):
-#         for j in range(len(Vj)):
-#             matrix_value = Vi - Vj
-#             output_matrix[i,j]= matrix_value
-#         return output_matrix
-#
-# print (compare_vicinities(V1,V2))
-
 for p, q in itertools.product(V1, V2):
-    print(p, q)
+    #print(p, q)
 
     #for p, q in zip(V1, V2):   # doesn't match ALL possible combinations
-
 
     def pairing_score(p, q):
         #output = []
@@ -45,16 +36,73 @@ for p, q in itertools.product(V1, V2):
         #output.append(score)
         return output
 
-    print(pairing_score(p, q))
+    #print(pairing_score(p, q))
+
+    # determine max pairing score (it's 1)
+    if pairing_score(p, q) >= 0.9:
+        print(pairing_score(p, q))
 
     # Create matrix P x Q of all the pairing scores
 
-def compare_vicinities(Vi,Vj):
-    output_matrix = np.zeros((len(Vi),len(Vj)))
+
+def vicinities_matrix(Vi, Vj):
+    output_matrix = np.zeros((len(Vi), len(Vj)))
     for i in range(len(Vi)):
         for j in range(len(Vj)):
             matrix_value = pairing_score(Vi[i], Vj[j])
-            output_matrix[i,j]= matrix_value
+            output_matrix[i, j] = matrix_value
     return output_matrix
 
-print (compare_vicinities(V1,V2))
+
+print("This is the comparison matrix Vicinity 1 to Vicinity 2: ")
+print(vicinities_matrix(V1, V2))
+
+# create a new vicinity to comparison
+
+V3 = [(4, 2, 0.3), (2, 3.3, -2), (-2, 1, 1), (-4, 0, 1)]
+V4 = [(1.1, 2.2, 3.3), (-1.1, -2.2, -3.3), (1.1, 2.2, 3.3)]
+
+print("This is the comparison matrix of Viciinty 3 to Vicinity 4: ")
+print(vicinities_matrix(V3, V4))
+
+#for pi, qj in itertools.product(V3, V4):
+#print (pairing_score(pi,qj))
+
+# Implement Hungarian Method to determine the best pairings between minutiae
+
+
+def overall_pair_score(matrix):
+    # Create matrix filled with values = 1 - pairing score, in order to find maximums
+    inv_matrix = []
+    for row in matrix:
+        #print(row)
+        inv_row = []
+        for col in row:
+            inv_row += [1 - col]
+        inv_matrix += [inv_row]
+        #print(inv_matrix)
+
+    m = Munkres()  # Munkres = Hungarian
+    indexes = m.compute(inv_matrix)
+    #print_matrix(inv_matrix, msg='compute highest pairing scores')
+    total = 0
+    for row, col in indexes:
+        val = inv_matrix[row][col]
+        total += val
+        print(f'({row}{col}) -> {val}')
+    print(f'sum of scores: {total}')
+    #return inv_matrix, total
+
+
+# compute total pairing score for vicinities 3 and 4
+M2 = vicinities_matrix(V3, V4)
+print("This is the overall_pair_score of V3 and V4")
+print(overall_pair_score(M2))
+
+# compute total pairing score for vicinities 1 and 2
+M1 = (vicinities_matrix(V1, V2))
+print("This is the overall_pair_score of V1 and V2")
+print(overall_pair_score(M1))
+'''
+Total Pairing Score is used to compare vicinities of the same fingerprint or of different fingerprint
+'''
