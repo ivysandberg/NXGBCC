52a86b4be036f44006005c882fb42dbe69599088
ivysandberg <ins5kx@virginia.edu>
2019-06-13 16:36:29 -0400

    def radius and comprehend pairing score

--- compare_vicinities.py
+++ compare_vicinities.py
@@ -4,9 +4,9 @@ import itertools
 import math
 from munkres import Munkres, print_matrix
 
-from yapf.yapflib.yapf_api import FormatFile
-FormatFile(
-    "/Users/ivysandberg/dmc/NXGBCC/dev/compare_vicinities.py", in_place=True)
+# from yapf.yapflib.yapf_api import FormatFile
+# FormatFile(
+#     "/Users/ivysandberg/dmc/NXGBCC/dev/compare_vicinities.py", in_place=True)
 ''' Compare Vicinities '''
 
 V1 = [(3, 2, 0.5), (-4, 1.2, 3.4), (5, -2, 4.4), (1, -1, 1), (3.6, 2, 0.5),
@@ -39,8 +39,10 @@ for p, q in itertools.product(V1, V2):
     #print(pairing_score(p, q))
 
     # determine max pairing score (it's 1)
-    if pairing_score(p, q) >= 0.9:
-        print(pairing_score(p, q))
+    # if pairing_score(p, q) >= 0.9:
+    #     print(pairing_score(p, q))
+    # if pairing_score(p,q) >= 0.1:
+    #     print(p,q)
 
     # Create matrix P x Q of all the pairing scores
 
@@ -59,8 +61,8 @@ print(vicinities_matrix(V1, V2))
 
 # create a new vicinity to comparison
 
-V3 = [(4, 2, 0.3), (2, 3.3, -2), (-2, 1, 1), (-4, 0, 1)]
-V4 = [(1.1, 2.2, 3.3), (-1.1, -2.2, -3.3), (1.1, 2.2, 3.3)]
+V3 = [(0,0,0),(4, 2, 0.3), (2, 3.3, -2), (-2, 1, 1), (-4, 0, 1)]
+V4 = [(1.1, 2.2, 3.3), (-1.1, -2.2, -3.3), (1.1, 2.2, 3.3), (1,1,1)]
 
 print("This is the comparison matrix of Viciinty 3 to Vicinity 4: ")
 print(vicinities_matrix(V3, V4))
@@ -71,8 +73,8 @@ print(vicinities_matrix(V3, V4))
 # Implement Hungarian Method to determine the best pairings between minutiae
 
 
-def overall_pair_score(matrix):
-    # Create matrix filled with values = 1 - pairing score, in order to find maximums
+def Hungarian(matrix):
+    # Create matrix filled with values = 1 - pairing score, in order to find maximums, bc Hungarian is designed to minimize cost (in the assignment problem)
     inv_matrix = []
     for row in matrix:
         #print(row)
@@ -90,19 +92,28 @@ def overall_pair_score(matrix):
         val = inv_matrix[row][col]
         total += val
         print(f'({row}{col}) -> {val}')
-    print(f'sum of scores: {total}')
+    return (f'sum of scores: {total}')
     #return inv_matrix, total
 
 
 # compute total pairing score for vicinities 3 and 4
 M2 = vicinities_matrix(V3, V4)
-print("This is the overall_pair_score of V3 and V4")
-print(overall_pair_score(M2))
+print("This is the Hungarian alg on V3 and V4")
+print(Hungarian(M2))
 
 # compute total pairing score for vicinities 1 and 2
 M1 = (vicinities_matrix(V1, V2))
-print("This is the overall_pair_score of V1 and V2")
-print(overall_pair_score(M1))
+print("This is the Hungarian alg on V1 and V2")
+print(Hungarian(M1))
 '''
 Total Pairing Score is used to compare vicinities of the same fingerprint or of different fingerprint
 '''
+
+
+# run test to compare to vicinities that are the same (result: sum of pair scores = 0)
+# this is bc we do 1-pairing score to find maxs (best pairs)
+V4 = [(1.1, 2.2, 3.3), (-1.1, -2.2, -3.3), (1.1, 2.2, 3.3)]
+V5 = [(1.1, 2.2, 3.3), (-1.1, -2.2, -3.3), (1.1, 2.2, 3.3)]
+
+M3 = (vicinities_matrix(V4, V5))
+print (Hungarian(M3))
--- compress_vicinity_def.py
+++ compress_vicinity_def.py
@@ -3,18 +3,45 @@ import pandas as pd
 import math
 import itertools
 
-# from yapf.yapflib.yapf_api import FormatFile
-# FormatFile(
-#     "/Users/ivysandberg/dmc/NXGBCC/dev/compress_vicinity_def.py",
-#     in_place=True)
+from yapf.yapflib.yapf_api import FormatFile
+FormatFile(
+    "/Users/ivysandberg/dmc/NXGBCC/dev/compress_vicinity_def.py",
+    in_place=True)
 
 # list of all the minutiae points
 list_of_minutiae = [(3, 2, 0.5), (-4, 1.2, 3.4), (5, -2, 4.4), (1, -1, 1),
                     (3.6, 2, 0.5), (1, 1, 1), (-2, -0.3, 0), (1, 2, -4)]
 
-#print(itertools.combinations_with_replacement(list_of_minutiae, 2))
+# define a radius
+center_minutiae = list_of_minutiae[0]
+other_minutiae = list_of_minutiae[1:]
+
+
+def vicinity_within_radius(minutiae):
+    list_of_minutiae_in_r = []
+    r = 2.5
+    for i in other_minutiae:
+        x = i[0]
+        y = i[1]
+        if x**2 + y**2 <= r**2:
+            list_of_minutiae_in_r.append(other_minutiae)
+    return list_of_minutiae_in_r
+
+
+print(
+    "This is a list of all the mjs within radius = 2.5 when mi = (3, 2, 0.5) : "
+)
+print(vicinity_within_radius(list_of_minutiae))
+
+# Create a change of coords for every combination of mi (center minutiae) and mj (other minutiae)
+# print(itertools.combinations_with_replacement(list_of_minutiae, 2)) = every combination of coordinates
 
 for mi, mj in itertools.combinations_with_replacement(list_of_minutiae, 2):
+    print("This is the (mi, mj) coordinate being changed")
+    print(mi, mj)
+
+    # Functions needed to determine the change of coordinates when one coord is translated & rotated to the origin
+
 
     def distance(mi, mj):
         dist = math.sqrt((mj[0] - mi[0])**2 + (mj[1] - mi[1])**2)
@@ -39,15 +66,17 @@ for mi, mj in itertools.combinations_with_replacement(list_of_minutiae, 2):
     # print (mi, mj)
     # print ("Distance: ", distance(mi,mj))
     # print ("Angle: ", angle_between(mi,mj))
+    ''' Find the new X coordinate '''
 
     # define X coordinate of Vicinity 1 (corresponding to mi and mj)
     X = distance(mi, mj) * (math.cos(angle_between(mi, mj) - mi[2]))
     #print("X: ", X)
-
+    ''' Find the new Y coordinate '''
     # define Y coordinate
     Y = distance(mi, mj) * (math.sin(angle_between(mi, mj) - mi[2]))
 
     #print("Y: ", Y)
+    ''' Find the new theta coordinate '''
 
     # define theta
     def theta(mi, mj):
@@ -66,5 +95,5 @@ for mi, mj in itertools.combinations_with_replacement(list_of_minutiae, 2):
         return output
 
     #print("Vicintiy: ", vicinity(mi, mj))
-
+    print("This is the change of coord for the mj point with mi at (0,0,0)")
     print(vicinity(mi, mj))
